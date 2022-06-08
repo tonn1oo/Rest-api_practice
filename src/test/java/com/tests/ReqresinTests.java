c0fdd0582beaeabcd144a85b770c50d133cd61d7
tonn1oo <ant.vlasov.87@gmail.com>
2022-06-09 02:30:53 +0300

    added reqresinTests

---
+++ src/test/java/com/tests/ReqresinTests.java
@@ -0,0 +1,44 @@
+package com.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresinTests {
+    @Test
+    void loginTest(){
+        String body = "{ \"email\": \"eve.holt@reqres.in\"," +
+                "\"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+    @Test
+    void missingPasswordloginTest(){
+        String body = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
